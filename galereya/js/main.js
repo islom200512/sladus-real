5e0ede13ecc3dd6c289377b1d0a1012c4ee82ad8
islom200512 <101330876+islom200512@users.noreply.github.com>
2022-05-11 13:11:49 +0500

    Add files via upload

---
+++ galereya/js/main.js
@@ -0,0 +1,36 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+
+    nav: true,
+    dots: true,
+
+    responsive: {
+        0: {
+            items: 3
+        },
+        626: {
+            items: 3
+        },
+        900: {
+            items: 3
+        }
+    }
+})
+$('.owl-carousel.two').owlCarousel({
+    loop: false,
+
+    nav: true,
+    dots: true,
+
+    responsive: {
+        0: {
+            items: 4
+        },
+        626: {
+            items: 4
+        },
+        900: {
+            items: 4
+        }
+    }
+})
